4e6ac46b47f8ac294653f129c87a01d9cbb1f377
lavinaIgnacio <ucone92@gmail.com>
2019-05-24 02:00:30 -0700

    trash adde

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$968badedbc3ab741$c293537f16163c05fb227c829d2de129bdf1683f"
-=======
-password="pbkdf2(1000,20,sha512)$8998216010ee84b2$010c0a3b49a04ea9d0cc73e12b817fa95a4701db"
->>>>>>> temp-branch
+password="pbkdf2(1000,20,sha512)$8c4c4ba87b1998a3$f7e61119769ed6e07090a7f5ec7ba98682f07f79"
