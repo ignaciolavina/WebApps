664e9b836e5cca7aa3aad639ac22571e3948cdb4
lavinaIgnacio <ucone92@gmail.com>
2019-05-28 10:40:45 -0700

    now the page displays the stars, still not storing them correctly and not retrieveing the average correctly

--- web2py/applications/hw5_v3/controllers/api.py
+++ web2py/applications/hw5_v3/controllers/api.py
@@ -2,13 +2,16 @@ def get_logged_in_user():
     user = None if auth.user is None else auth.user.email
     return response.json(dict(user=user))
 
-# def get_all_books():
-#     products = db(db.product).select()
-#     return response.json(dict(products=products))
-
-
 def get_all_products():
     products = db(db.product).select()
+    for product in products:
+        product.avg_rating = 0
+        sum = 0
+        reviews = db((db.review.product_id == product.id) & (db.review.rating >= 0)).select()
+        for review in reviews:
+            sum += review.rating
+        product.avg_rating = sum / len(reviews)
+
     return response.json(dict(products=products))
 
 @auth.requires_login()
